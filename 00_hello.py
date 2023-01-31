41cf20158fc4463a4fbddcc8e56712c418c7d6bc
Mayi <mayi.eliza3699@hotmail.com>
2023-01-31 17:00:13 -0500

    added data types

---
+++ 00_hello.py
@@ -0,0 +1,27 @@
+# Comment
+
+# Print on console
+print('Hello World!')
+
+'''
+three 
+lines
+comment
+'''
+
+# print data type
+print(type('Hello World!')) # 'str' data type
+
+# variables
+
+first_name = 'Mayi'
+last_name = 'Contreras'
+country = 'Ecuador'
+
+print(first_name, last_name, country)
+
+# input
+
+age = input('How old are you? ')
+
+print('Your age is', age)
---
+++ 01_data_types.py
@@ -0,0 +1,16 @@
+# Integers
+
+print(type(1))
+
+# Floats
+
+print(type(2.0))
+
+# Strings
+
+print(type('Hello World!'))
+
+# Booleans
+
+print(type(True))
+print(type(False))
